294ea723055b49d783cec9452681f4d44125bec5
Tahkayun <Tahkayun@users.noreply.github.com>
2019-02-06 22:37:02 +0100

    Initial commit

---
+++ app/src/main/java/ioscript/passwordgenerator/SettingsFragment.java
@@ -0,0 +1,34 @@
+package ioscript.passwordgenerator;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+     setPreferencesFromResource(R.xml.preferences, s);
+
+
+    }
+
+}
